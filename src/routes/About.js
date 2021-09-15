44b9918529a9ea905bcbf1cfe0824437f1324a47
Secret8890 <gml8890@naver.com>
2021-09-15 20:00:07 +0900

    Detail page and Finally Commit

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About(props) {
+    console.log(props)
+  return (
+    <div className="about__container">
+    <span> "Freedom is the freedom to say that two make four. If that iss
+      granted, all else follows." 
+      </span>
+      <span> - George Orwell, 1984.</span>
+      </div>
+      );
+}
+
+export default About;
